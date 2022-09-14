61816f5dc03b8201d8de4d647b88c1025b3b8013
luis <lmatosgarcia2@gmail.com>
2022-09-13 22:37:21 -0400

    Full-Text Search

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -199,7 +199,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	// Construct the SQL query to retrieve all movie records.
 	query := `SELECT id, created_at, title, year, runtime, genres, version
           FROM movies
-          WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+          WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
           AND (genres @> $2 OR $2 = '{}')`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
